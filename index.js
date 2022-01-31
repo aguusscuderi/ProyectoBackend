58076ff9795154cd056e538aa79f86446ee45fab
aguusscuderi <aguusscuderi9@gmail.com>
2022-01-31 16:24:42 -0300

    upload

--- index.js
+++ index.js
@@ -14,10 +14,14 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs')
-app.set('views', './views')
+app.set('views', path.join(__dirname, 'views'))
+
+/*app.get('/', (req,res)=>{
+    res.redirect('/api/index')
+})*/
 
 app.get('/', (req,res)=>{
-    res.redirect('/api/index')
+    res.render('index')
 })
 
 serverRouter(app)

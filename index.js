89df85ec537b8be7a5bef022e5af1139ec6bc1e4
aguusscuderi <aguusscuderi9@gmail.com>
2022-02-02 13:27:57 -0300

    heroku up

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ const db_connection = require('./config/db')
 const db_atlas_connection = require('./config/db_atlas')
 //let {productCreateSchema} = require('./components/users/schema/productosSchema')
 
-const PORT = 8080
+const PORT = process.env.PORT || 5000
 
 //app.use(cors("*"))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+//app.use(express.json())
+//app.use(express.urlencoded({extended: true}))
 app.use('/', express.static(path.join(__dirname, 'public')))
-app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
 
 /*app.get('/', (req,res)=>{
     res.redirect('/api/index')
